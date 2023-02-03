25122f85ec00086e6acc2c692106e3b3041dada8
Eunha Jeong <eunhaay@gmail.com>
2023-02-03 23:12:59 +0900

    Feat: 댓글 도메인 추가

---
+++ src/main/java/com/eunhasoo/bookclub/review/domain/Comment.java
@@ -0,0 +1,43 @@
+package com.eunhasoo.bookclub.review.domain;
+
+import com.eunhasoo.bookclub.common.BaseTime;
+import com.eunhasoo.bookclub.user.domain.User;
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Getter
+@Entity
+public class Comment extends BaseTime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_id")
+    private Review review;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    protected Comment() {
+    }
+
+    @Builder
+    public Comment(String content, Review review, User user) {
+        this.content = content;
+        this.review = review;
+        this.user = user;
+    }
+}
