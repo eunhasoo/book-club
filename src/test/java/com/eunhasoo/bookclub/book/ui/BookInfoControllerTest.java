637a8538fc26cf15b591286d7ffcf00d4f2836b9
Eunha Jeong <eunhaay@gmail.com>
2023-02-06 17:08:34 +0900

    Test: 책 정보 API에 회원 인증을 위한 코드 추가

--- src/test/java/com/eunhasoo/bookclub/book/ui/BookInfoControllerTest.java
+++ src/test/java/com/eunhasoo/bookclub/book/ui/BookInfoControllerTest.java
@@ -1,9 +1,14 @@
 package com.eunhasoo.bookclub.book.ui;
 
+import com.eunhasoo.bookclub.auth.CustomUserDetails;
+import com.eunhasoo.bookclub.auth.CustomUserDetailsService;
+import com.eunhasoo.bookclub.auth.jwt.TokenProvider;
 import com.eunhasoo.bookclub.book.domain.BookInfo;
 import com.eunhasoo.bookclub.book.domain.BookInfoRepository;
 import com.eunhasoo.bookclub.book.ui.request.BookInfoCreate;
-import com.eunhasoo.bookclub.book.ui.request.BookInfoSearch;
+import com.eunhasoo.bookclub.helper.Fixture;
+import com.eunhasoo.bookclub.user.domain.User;
+import com.eunhasoo.bookclub.user.domain.UserRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
@@ -11,11 +16,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static com.eunhasoo.bookclub.helper.Fixture.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -37,9 +47,19 @@ class BookInfoControllerTest {
     @Autowired
     private ObjectMapper om;
 
+    @MockBean
+    private TokenProvider tokenProvider;
+
+    @MockBean
+    private CustomUserDetailsService userDetailsService;
+
+    @Autowired
+    private UserRepository userRepository;
+
     @AfterEach
     void clear() {
         bookInfoRepository.deleteAll();
+        userRepository.deleteAll();
     }
 
     @Test
@@ -47,12 +67,16 @@ class BookInfoControllerTest {
     void find_with_exist_isbn() throws Exception {
         // given
         BookInfo bookInfo = bookInfoRepository.save(bookInfo());
+        User user = userRepository.save(Fixture.user());
+
+        when(tokenProvider.validateToken(any())).thenReturn(true);
+        when(tokenProvider.createToken(anyLong())).thenReturn("token");
+        when(tokenProvider.getUserIdFromToken(any())).thenReturn(user.getId());
+        when(userDetailsService.loadUserByUserId(any())).thenReturn(CustomUserDetails.create(user));
 
         // expected
-        BookInfoSearch bookInfoSearch = new BookInfoSearch(bookInfo.getIsbn());
-        String json = om.writeValueAsString(bookInfoSearch);
-
         mockMvc.perform(get(BOOK_INFO_API + "/search")
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer token")
                         .param("isbn", bookInfo.getIsbn()))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.isExist").value(true))
@@ -64,10 +88,17 @@ class BookInfoControllerTest {
     @DisplayName(BOOK_INFO_API + "/search GET 요청시 ISBN이 일치하는 책 정보를 찾지 못하면 isExist 값을 false로 응답을 생성한다.")
     void find_with_not_exist_isbn() throws Exception {
         // given
+        User user = userRepository.save(Fixture.user());
         String notExistISBN = "9999999999999";
+
+        when(tokenProvider.validateToken(any())).thenReturn(true);
+        when(tokenProvider.createToken(anyLong())).thenReturn("token");
+        when(tokenProvider.getUserIdFromToken(any())).thenReturn(user.getId());
+        when(userDetailsService.loadUserByUserId(any())).thenReturn(CustomUserDetails.create(user));
 
         // expected
         mockMvc.perform(get(BOOK_INFO_API + "/search")
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer token")
                         .param("isbn", notExistISBN))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.isExist").value(false))
@@ -79,9 +110,16 @@ class BookInfoControllerTest {
     void get_book_info_with_id() throws Exception {
         // given
         BookInfo bookInfo = bookInfoRepository.save(bookInfo());
+        User user = userRepository.save(Fixture.user());
+
+        when(tokenProvider.validateToken(any())).thenReturn(true);
+        when(tokenProvider.createToken(anyLong())).thenReturn("token");
+        when(tokenProvider.getUserIdFromToken(any())).thenReturn(user.getId());
+        when(userDetailsService.loadUserByUserId(any())).thenReturn(CustomUserDetails.create(user));
 
         // expected
-        mockMvc.perform(get(BOOK_INFO_API + "/{bookInfoId}", bookInfo.getId()))
+        mockMvc.perform(get(BOOK_INFO_API + "/{bookInfoId}", bookInfo.getId())
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer token"))
                 .andExpect(status().isOk())
                 .andExpectAll(
                     jsonPath("$.id").value(bookInfo.getId()),
@@ -100,7 +138,15 @@ class BookInfoControllerTest {
     @Test
     @DisplayName(BOOK_INFO_API + " GET 요청시 책 정보를 찾지 못하면 404 응답 코드와 메시지를 생성한다.")
     void get_book_info_not_found() throws Exception {
-        mockMvc.perform(get(BOOK_INFO_API + "/{bookInfo}", 1000L))
+        User user = userRepository.save(Fixture.user());
+
+        when(tokenProvider.validateToken(any())).thenReturn(true);
+        when(tokenProvider.createToken(anyLong())).thenReturn("token");
+        when(tokenProvider.getUserIdFromToken(any())).thenReturn(user.getId());
+        when(userDetailsService.loadUserByUserId(any())).thenReturn(CustomUserDetails.create(user));
+
+        mockMvc.perform(get(BOOK_INFO_API + "/{bookInfo}", 1000L)
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer token"))
                 .andExpect(status().isNotFound())
                 .andExpectAll(
                         jsonPath("$.statusCode").value("404"),
@@ -114,6 +160,12 @@ class BookInfoControllerTest {
     void create_book_info() throws Exception {
         // given
         BookInfo bookInfo = bookInfo();
+        User user = userRepository.save(Fixture.user());
+
+        when(tokenProvider.validateToken(any())).thenReturn(true);
+        when(tokenProvider.createToken(anyLong())).thenReturn("token");
+        when(tokenProvider.getUserIdFromToken(any())).thenReturn(user.getId());
+        when(userDetailsService.loadUserByUserId(any())).thenReturn(CustomUserDetails.create(user));
 
         BookInfoCreate bookInfoCreate = BookInfoCreate.builder()
                 .name(bookInfo.getName())
@@ -127,6 +179,7 @@ class BookInfoControllerTest {
 
         // expected
         mockMvc.perform(post(BOOK_INFO_API)
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer token")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(om.writeValueAsString(bookInfoCreate)))
                 .andExpect(status().isCreated())
